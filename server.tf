264e155be7e868424ec40eec1624415a4d17f532
Raman Khanna <89792896+ramannkhanna2@users.noreply.github.com>
2023-04-14 15:58:25 +0530

    Update server.tf

--- server.tf
+++ server.tf
@@ -4,10 +4,10 @@ region = "us-east-1"
 resource "aws_instance" "myawsserver" {
   ami = "ami-0b0dcb5067f052a63"
   instance_type = "t2.micro"
-  key_name = "raman-import"
+  key_name = "raman-virginia"
 
   tags = {
-    Name = "Raman-DevOps-Cisco-batch-server"
+    Name = "Raman-DevOps-batch-server"
     env = "Production"
     owner = "Raman Khanna"
   }
