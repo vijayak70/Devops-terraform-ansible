4dcb29cc55688b57f5df966f2cbda80dc055f4a1
Raman Khanna <89792896+ramannkhanna2@users.noreply.github.com>
2023-05-24 19:24:05 +0530

    Update server.tf

--- server.tf
+++ server.tf
@@ -4,7 +4,7 @@ region = "us-east-1"
 resource "aws_instance" "myawsserver" {
   ami = "ami-0b0dcb5067f052a63"
   instance_type = "t2.micro"
-  key_name = "raman-import"
+  key_name = "synechron-key"
 
   tags = {
     Name = "Raman-DevOps-batch-server"
