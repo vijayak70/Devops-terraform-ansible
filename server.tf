6372259a7b292b1b68079fec6a9f2e4b6a792ffa
Raman Khanna <89792896+ramannkhanna2@users.noreply.github.com>
2024-02-16 14:25:28 +0530

    Update server.tf

--- server.tf
+++ server.tf
@@ -18,7 +18,7 @@ resource "aws_instance" "myawsserver" {
   tags = {
     Name = "gagan-DevOps-batch-server"
     env = "Production"
-    owner = "Gagan Deep"
+    owner = "Gagandeep"
   }
   provisioner "local-exec" {
     command = "echo The servers IP address is ${self.public_ip} && echo ${self.public_ip} > /tmp/inv"
