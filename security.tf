ef68b3d7f8a4ae98992a99df4f81afa4d3352118
vijayak70 <kevia_2006@hotmail.com>
2024-09-27 14:50:37 +0530

    Create security.tf

---
+++ security.tf
@@ -0,0 +1,28 @@
+resource "aws_security_group" "allow_ssh" {
+  name        = "vijay_allow_ssh"
+  description = "Allow SSH inbound traffic"
+  vpc_id      = "vpc-00dae5f3df962676d"
+
+  ingress {
+    description = "SSH into VPC"
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+  
+ ingress {
+    description = "SSH into VPC"
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+  egress {
+    description = "Outbound Allowed"
+    from_port   = 0
+    to_port     = 65535
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
